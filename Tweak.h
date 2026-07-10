1ea6bc0c71433c446fb52249eff608835239b7b3
Mark02 <Marchingegno02@protonmail.com>
2026-07-10 13:42:55 +0200

    Refactor Tweak.h by removing unnecessary code
    Removed unused interfaces and streamlined settings management.

--- Tweak.h
+++ Tweak.h
@@ -4,66 +4,19 @@
 #define LowContrastModeEnabledKey @"lowContrastMode_enabled"
 
 static inline BOOL IS_ENABLED(NSString *key) {
-    return [[NSUserDefaults standardUserDefaults] boolForKey:key];
+
+    NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
+
+    if ([defaults objectForKey:key] == nil) {
+        [defaults setBool:YES forKey:key];
+        return YES;
+    }
+
+    return [defaults boolForKey:key];
 }
-
-#pragma mark - Missing interfaces
-
-@interface YTCollectionViewCell : UICollectionViewCell
-@end
 
 @interface YTQTMButton : UIButton
 @end
-
-#pragma mark - Settings
-
-@interface YTSettingsCell : YTCollectionViewCell
-@end
-
-@interface YTSettingsSectionItem : NSObject
-
-@property(nonatomic, assign) BOOL hasSwitch;
-@property(nonatomic, assign) BOOL switchVisible;
-@property(nonatomic, assign) BOOL on;
-@property(nonatomic, copy) BOOL (^switchBlock)(YTSettingsCell *, BOOL);
-@property(nonatomic, assign) int settingItemId;
-
-- (instancetype)initWithTitle:(NSString *)title
-             titleDescription:(NSString *)titleDescription;
-
-+ (instancetype)itemWithTitle:(NSString *)title
-             titleDescription:(NSString *)titleDescription
-      accessibilityIdentifier:(NSString *)identifier
-              detailTextBlock:(NSString *(^)(void))detailTextBlock
-                  selectBlock:(BOOL (^)(YTSettingsCell *, NSUInteger))selectBlock;
-
-+ (instancetype)switchItemWithTitle:(NSString *)title
-                   titleDescription:(NSString *)titleDescription
-            accessibilityIdentifier:(NSString *)identifier
-                           switchOn:(BOOL)switchOn
-                        switchBlock:(BOOL (^)(YTSettingsCell *, BOOL))switchBlock
-                      settingItemId:(NSInteger)itemId;
-
-@end
-
-@interface YTSettingsGroupData : NSObject
-@property(nonatomic, assign) NSInteger type;
-@end
-
-@interface YTSettingsViewController : UIViewController
-
-- (void)setSectionItems:(NSArray *)items
-            forCategory:(NSUInteger)category
-                  title:(NSString *)title
-       titleDescription:(NSString *)description
-           headerHidden:(BOOL)hidden;
-
-@end
-
-@interface YTSettingsSectionItemManager : NSObject
-@end
-
-#pragma mark - Existing interfaces
 
 @interface _ASCollectionViewCell : UICollectionViewCell
 - (id)node;
